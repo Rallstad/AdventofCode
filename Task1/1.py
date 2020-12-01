465a6ce84f534743eede4608cd229bf3262fd8a4
Rallstad <haraldkjorholt@gmail.com>
2020-12-01 14:16:48 +0100

    add readFile function

--- Task1/1.py
+++ Task1/1.py
@@ -1,27 +1,31 @@
-inputFile = open('input.txt', 'r')
-data = inputFile.readlines()
-
-
+def readFile():
+	inputFile = open('input.txt', 'r')
+	data = inputFile.readlines()
+	num = [int(line) for line in data]
+	return num
+	
 def task1a():
-	for i in range(len(data)):
-		for j in range(i+1, len(data)):
-			value = int(data[i]) + int(data[j])
+	num = readFile()
+	for i in range(len(num)):
+		for j in range(i+1, len(num)):
+			value = num[i] + num[j]
 			if value == 2020:
-				print("%d + %d = %d" %(int(data[i]), int(data[j]), value))
-				print("product: %d" %(int(data[i]) * int(data[j])))
+				print("%d + %d = %d" %(num[i], num[j], value))
+				print("product: %d" %(num[i] * num[j]))
 
 def task1b():
-	for i in range(len(data)):
-		for j in range(i+1, len(data)):
-			for k in range(j+1, len(data)):
-				value = int(data[i]) + int(data[j]) + int(data[k])
+	num = readFile()
+	for i in range(len(num)):
+		for j in range(i+1, len(num)):
+			for k in range(j+1, len(num)):
+				value = num[i] + num[j] + num[k]
 				if value == 2020:
-					print("%d + %d + %d = %d" %(int(data[i]), int(data[j]), int(data[k]), value))
-					print("product: %d" %(int(data[i]) * int(data[j]) * int(data[k])))
+					print("%d + %d + %d = %d" %(num[i], num[j], num[k], value))
+					print("product: %d" %(num[i] * num[j] * num[k]))
 
 print("task 1a")
 task1a()
-	
+
 print("task 1b")
 task1b()
 
